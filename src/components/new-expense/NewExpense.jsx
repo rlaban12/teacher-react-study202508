9fe98b8d3f3aa84e5ec40b7488a41e5e3c8dd57e
soongu <naong526@naver.com>
2025-08-26 12:14:48 +0900

    3-2-3. 조건부 렌더링 연습문제 답안

--- src/components/new-expense/NewExpense.jsx
+++ src/components/new-expense/NewExpense.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = ({ onSave }) => {
 
-  const formComponent = <ExpenseForm onAdd={onSave} />;
-  const buttonComponent = <button>새로운 지출 추가하기</button>;
+  // 화면상태를 제어하기위한 논리상태변수
+  const [toggle, setToggle] = useState(false);
+
+  const formComponent = <ExpenseForm onAdd={onSave} onCancel={() => setToggle(false)} />;
+  const buttonComponent = <button onClick={() => setToggle(true)}>새로운 지출 추가하기</button>;
 
   return (
     <div className='new-expense'>
-      { formComponent }
+      { toggle ? formComponent : buttonComponent }
     </div>
   );
 };
