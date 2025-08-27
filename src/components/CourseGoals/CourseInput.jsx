98c717915795e7e3ac49e8d69d04fcffd705ecce
soongu <naong526@naver.com>
2025-08-27 09:46:01 +0900

    4-1-1. 동적 인라인 스타일 조작

--- src/components/CourseGoals/CourseInput.jsx
+++ src/components/CourseGoals/CourseInput.jsx
@@ -6,23 +6,47 @@ const CourseInput = ({ onAdd }) => {
 
   // 목표인풋에 입력한 값 상태관리
   const [enteredText, setEnteredText] = useState('');
+  // 입력값에 오류가 있는지 여부를 상태관리
+  const [isValid, setIsValid] = useState(null);
 
   const submitHandler = e => {
     e.preventDefault();
+
+    // 입력값 검증
+    if (!enteredText.trim()) {
+      setIsValid(false);
+      return;
+    }
+
     // console.log(`입력값: ${enteredText}`);
     onAdd(enteredText);
 
     setEnteredText('');
+    setIsValid(true);
   };
 
+  const goalInputHandler = e => {
+    const inputValue = e.target.value;
+
+    // 입력값 검증
+    if (inputValue.trim()) {
+      setIsValid(true);
+    }
+
+    setEnteredText(inputValue);
+  };
   return (
     <form onSubmit={submitHandler}>
       <div className='form-control'>
-        <label>나의 목표</label>
+        <label style={{ color: isValid !== false ? 'black' : 'red' }}>나의 목표</label>
         <input
           type='text'
-          onInput={e => setEnteredText(e.target.value)}
+          onInput={goalInputHandler}
           value={enteredText}
+          style={{
+            background: isValid !== false ? 'transparent' : 'salmon',
+            borderColor: isValid !== false ? 'black' : 'red'
+          }}
         />
       </div>
       <Button type='submit'>목표 추가하기</Button>
