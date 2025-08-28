2a417bdbe4f41bdc4096c2aafd17936658f25148
soongu <naong526@naver.com>
2025-08-28 12:27:53 +0900

    6장 복습예제. 1-타이머게임 템플릿 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
-import MainHeader from './components/SideEffect/MainHeader.jsx';
-import Home from './components/SideEffect/Home.jsx';
-import Login from './components/SideEffect/Login.jsx';
+import Player from './components/TimerGame/Player.jsx';
 
 const App = () => {
 
-  // 로그인 상태 체크
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  // 토큰 검사
-  // console.log('토큰 검사!');
-  // const token = localStorage.getItem('token');
-  // if (token === '1') {
-  //   setIsLoggedIn(true); // 리 렌더링 발생
-  // }
-
-  // side effect (서버에서 데이터를 가져오거나 외부 비동기작업) 처리를 위한 훅(리액트 함수)
-  useEffect(() => {
-    // console.log('useEffect가 실행됨!');
-    const token = localStorage.getItem('token');
-    if (token === '1') {
-      setIsLoggedIn(true); // 리 렌더링 발생
-    }
-  }, []);
-
-  const handleLogin = (email, password) => {
-
-    // 로그인 검증
-    if (email === 'abc@def.com' && password === '12345678') {
-      localStorage.setItem('token', '1');
-      setIsLoggedIn(true); // 리 렌더링 발생
-    } else {
-      alert('로그인 실패!');
-    }
-  };
-
-  // console.log('렌더링 수행!');
-
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    setIsLoggedIn(false);
-  };
-
   return (
     <>
-      <MainHeader onLogout={handleLogout} />
-      <main>
-        {isLoggedIn && <Home/>}
-        {!isLoggedIn && <Login onLogin={handleLogin} />}
-      </main>
+      <Player />
     </>
   );
 };
