5dafe672723f6559b838464b1edbe6beaa02ab89
soongu <naong526@naver.com>
2025-08-28 10:04:10 +0900

    5-2-2. 사용자 추가와 목록 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import TodoTemplate from './components/Todos/TodoTemplate.jsx';
 import AddUsers from './components/Users/AddUsers.jsx';
 import UserList from './components/Users/UserList.jsx';
 
 const App = () => {
 
+  // 회원들이 저장될 배열
+  const [userList, setUserList] = useState([]);
+
+  // 입력한 회원정보를 가져오는 함수
+  const addUserInfo = (user) => {
+    setUserList([...userList, user]);
+  };
+
   return (
     <>
-      <AddUsers />
-      <UserList />
+      <AddUsers onAddUser={addUserInfo} />
+      <UserList users={userList} />
     </>
   );
 };
