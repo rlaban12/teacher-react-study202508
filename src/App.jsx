7791a7578cec1a4d0949f7e92e1a04d39266c45d
soongu <naong526@naver.com>
2025-08-28 11:44:42 +0900

    6-1-3. useEffect로 side effect처리

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import AddUsers from './components/Users/AddUsers.jsx';
-import UserList from './components/Users/UserList.jsx';
 import MainHeader from './components/SideEffect/MainHeader.jsx';
 import Home from './components/SideEffect/Home.jsx';
 import Login from './components/SideEffect/Login.jsx';
@@ -11,19 +9,43 @@ const App = () => {
   // 로그인 상태 체크
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // 토큰 검사
+  // console.log('토큰 검사!');
+  // const token = localStorage.getItem('token');
+  // if (token === '1') {
+  //   setIsLoggedIn(true); // 리 렌더링 발생
+  // }
+
+  // side effect (서버에서 데이터를 가져오거나 외부 비동기작업) 처리를 위한 훅(리액트 함수)
+  useEffect(() => {
+    console.log('useEffect가 실행됨!');
+    const token = localStorage.getItem('token');
+    if (token === '1') {
+      setIsLoggedIn(true); // 리 렌더링 발생
+    }
+  }, []);
+
   const handleLogin = (email, password) => {
 
     // 로그인 검증
     if (email === 'abc@def.com' && password === '12345678') {
-      setIsLoggedIn(true);
+      localStorage.setItem('token', '1');
+      setIsLoggedIn(true); // 리 렌더링 발생
     } else {
       alert('로그인 실패!');
     }
   };
 
+  // console.log('렌더링 수행!');
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
+  };
+
   return (
     <>
-      <MainHeader />
+      <MainHeader onLogout={handleLogout} />
       <main>
         {isLoggedIn && <Home/>}
         {!isLoggedIn && <Login onLogin={handleLogin} />}
