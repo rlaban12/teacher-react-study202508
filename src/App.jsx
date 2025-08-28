cf62b60964d39c2db18577d455c23a5737e12981
soongu <naong526@naver.com>
2025-08-28 14:21:23 +0900

    6장 복습예제. 4-타이머 챌린지 기본 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,10 @@ const App = () => {
     <>
       <Player />
       <div id="challenges">
-        <TimerChallenge />
+        <TimerChallenge title='Easy' targetTime={1} />
+        <TimerChallenge title='Not Easy' targetTime={5} />
+        <TimerChallenge title='Getting tough' targetTime={10} />
+        <TimerChallenge title='Pros only' targetTime={15} />
       </div>
     </>
   );
