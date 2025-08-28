21bf1527865d2e7920469fcf3848118b9efcd5be
soongu <naong526@naver.com>
2025-08-28 11:25:02 +0900

    6-1-2. 간단한 로그인 로직 구현

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,25 @@ import Login from './components/SideEffect/Login.jsx';
 
 const App = () => {
 
+  // 로그인 상태 체크
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = (email, password) => {
+
+    // 로그인 검증
+    if (email === 'abc@def.com' && password === '12345678') {
+      setIsLoggedIn(true);
+    } else {
+      alert('로그인 실패!');
+    }
+  };
 
   return (
     <>
       <MainHeader />
       <main>
-        <Home />
-        <Login />
+        {isLoggedIn && <Home/>}
+        {!isLoggedIn && <Login onLogin={handleLogin} />}
       </main>
     </>
   );
