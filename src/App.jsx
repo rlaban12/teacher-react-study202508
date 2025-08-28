56678fb17256eb809dbb82cc44920a8ceed50e02
soongu <naong526@naver.com>
2025-08-28 11:09:18 +0900

    6-1-1. side effect 템플릿

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,20 @@ import React, {useState} from 'react';
 import './App.css';
 import AddUsers from './components/Users/AddUsers.jsx';
 import UserList from './components/Users/UserList.jsx';
+import MainHeader from './components/SideEffect/MainHeader.jsx';
+import Home from './components/SideEffect/Home.jsx';
+import Login from './components/SideEffect/Login.jsx';
 
 const App = () => {
 
-  // 회원들이 저장될 배열
-  const [userList, setUserList] = useState([]);
-
-  // 입력한 회원정보를 가져오는 함수
-  const addUserInfo = (user) => {
-    setUserList([...userList, user]);
-  };
 
   return (
     <>
-      <AddUsers onAddUser={addUserInfo} />
-      <UserList users={userList} />
+      <MainHeader />
+      <main>
+        <Home />
+        <Login />
+      </main>
     </>
   );
 };
